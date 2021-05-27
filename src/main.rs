38e0ba9a911616f82617d7f9e1174cebe6e22e35
Bruno Grieder <bruno.grieder@cosmian.com>
2021-05-27 10:32:01 +0200

    using Slice::private_iput

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@
 #![cfg_attr(not(test), no_main)]
 
 use cosmian_std::prelude::scale_std::slice::Slice;
-use cosmian_std::scale::{self, println, ClearModp, SecretModp};
+use cosmian_std::scale::{self, println, Channel, ClearModp, SecretModp};
 use cosmian_std::{prelude::*, OutputRow};
 
 // Players
@@ -20,9 +20,8 @@ fn main() {
 
     'global: loop {
         // First fetch id of participant 0
-        let (mut id_s_modp_0, mut id_s_64_0) = match pr0.read_next_record() {
-            Some(res) => {
-                let id_s_modp = *res.get_unchecked(0);
+        let (mut id_s_modp_0, mut id_s_64_0) = match pr0.read_next_value() {
+            Some(id_s_modp) => {
                 let id_s_64 = SecretInteger::<64>::from(id_s_modp);
                 (id_s_modp, id_s_64)
             }
@@ -32,9 +31,8 @@ fn main() {
         };
 
         // First fetch id of participant 1
-        let (mut id_s_modp_1, mut id_s_64_1) = match pr1.read_next_record() {
-            Some(res) => {
-                let id_s_modp = *res.get_unchecked(0);
+        let (mut id_s_modp_1, mut id_s_64_1) = match pr0.read_next_value() {
+            Some(id_s_modp) => {
                 let id_s_64 = SecretInteger::<64>::from(id_s_modp);
                 (id_s_modp, id_s_64)
             }
@@ -61,9 +59,9 @@ fn main() {
             } else if i64::from(id_s_64_0.lt(id_s_64_1).reveal()) == 1 {
                 println!(" -> ID 0 < ID 1");
                 // Fetch next id_participant 0
-                match pr0.read_next_record() {
+                match pr0.read_next_value() {
                     Some(res) => {
-                        id_s_modp_0 = *res.get_unchecked(0);
+                        id_s_modp_0 = res;
                         id_s_64_0 = SecretInteger::<64>::from(id_s_modp_0);
                     }
                     None => {
@@ -72,9 +70,9 @@ fn main() {
                 };
             } else {
                 println!(" -> ID 0 > ID 1");
-                match pr1.read_next_record() {
+                match pr1.read_next_value() {
                     Some(res) => {
-                        id_s_modp_1 = *res.get_unchecked(0);
+                        id_s_modp_1 = res;
                         id_s_64_1 = SecretInteger::<64>::from(id_s_modp_1);
                     }
                     None => {
@@ -89,8 +87,10 @@ fn main() {
 
 struct PlayerReader<const P: u32> {
     player: Player<P>,
-    nb_records: u64,
-    current_record: u64,
+    nb_cols: u64,
+    current_col: u64,
+    values: Slice<SecretModp>,
+    current_index: u64,
     eof: bool,
 }
 
@@ -98,41 +98,57 @@ impl<const P: u32> PlayerReader<P> {
     pub fn new(player: Player<P>) -> PlayerReader<P> {
         PlayerReader {
             player,
-            nb_records: 0,
-            current_record: 0,
+            nb_cols: 0,
+            current_col: 0,
             eof: false,
+            values: Slice::uninitialized(0),
+            current_index: 0,
         }
     }
 
-    pub fn read_next_record(&mut self) -> Option<Slice<SecretModp>> {
+    pub fn read_next_value(&mut self) -> Option<SecretModp> {
         if self.eof {
             return None;
         }
-        if self.current_record >= self.nb_records {
-            let nb_records: ClearModp =
-                SecretModp::private_input(self.player, Channel::<0>).reveal();
-            self.nb_records = i64::from(nb_records) as u64;
-            if self.nb_records == 0 {
-                self.eof = true;
+        if self.current_index >= self.values.len() {
+            if self.read_next_column() {
+                self.current_index = 0;
+            } else {
                 return None;
             }
-            self.current_record = 0;
+        }
+        let value = *(self.values.get_unchecked(self.current_index));
+        self.current_index += 1;
+        Some(value)
+    }
+
+    pub fn read_next_column(&mut self) -> bool {
+        if self.current_col >= self.nb_cols {
+            if !self.read_next_record() {
+                return false;
+            }
+            self.current_col = 0;
         }
 
-        let num_cols = SecretModp::private_input(self.player, Channel::<1>).reveal();
-        let num_cols = i64::from(num_cols) as u64;
-
-        let mut row = Slice::uninitialized(num_cols);
-
-        for row_nb in 0..num_cols {
-            row.set(
-                row_nb,
-                &SecretModp::private_input(self.player, Channel::<2>),
-            );
+        let num_values = SecretModp::private_input(self.player, Channel::<1>).reveal();
+        let num_values = i64::from(num_values) as u64;
+        if num_values == 0 {
+            self.eof = true;
+            return false;
         }
 
-        self.current_record += 1;
+        self.values = Slice::private_input(num_values, self.player, Channel::<2>);
 
-        Some(row)
+        true
+    }
+
+    pub fn read_next_record(&mut self) -> bool {
+        let nb_cols: ClearModp = SecretModp::private_input(self.player, Channel::<0>).reveal();
+        self.nb_cols = i64::from(nb_cols) as u64;
+        if self.nb_cols == 0 {
+            self.eof = true;
+            return false;
+        }
+        true
     }
 }
